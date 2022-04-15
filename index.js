95550d733475f4a5dca8fefd5f3b741f46c1a72e
Boyd Zweers <boyd.zweers@alliander.com>
2022-04-15 08:29:02 +0200

    eoxp

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ const {
 
 const main = async () => {
   try {
-    /**
-     * We need to fetch all the inputs that were provided to our action
-     * and store them in variables for us to use.
-     **/
     const owner = core.getInput("owner", { required: true });
     const repo = core.getInput("repo", { required: true });
     const issueNumber = core.getInput("issueNumber", { required: true });
@@ -20,16 +16,6 @@ const main = async () => {
     const path = core.getInput("pathToIndex", { required: true });
     const failOnEOCP = core.getInput("failOnEOCP", { required: true });
 
-    console.log({ path, failOnEOCP });
-
-    /**
-     * Now we need to create an instance of Octokit which will use to call
-     * GitHub's REST API endpoints.
-     * We will pass the token as an argument to the constructor. This token
-     * will be used to authenticate our requests.
-     * You can find all the information about how to use Octokit here:
-     * https://octokit.github.io/rest.js/v18
-     **/
     const octokit = new github.getOctokit(token);
 
     const { data } = await octokit.rest.repos.getContent({
@@ -52,32 +38,28 @@ const main = async () => {
       const [quarter, year] = eocp.split("/");
       const eocpQuerter = Number(quarter.replace("Q", ""));
 
-      console.log({ currentYear, currentQuarter, quarter, year, eocpQuerter });
-
       let message = null;
       let code = null;
 
       if (Number(year) < currentYear) {
         code = 1;
-        message = "You need to update, eocp was last year of earlier";
+        message = "You need to update, eocp was last year of earlier.";
       } else if (Number(year) === currentYear && eocpQuerter < currentQuarter) {
         code = 2;
-        message = "You need to update, eocp was one or more quarters back";
+        message = "You need to update, eocp was one or more quarters back.";
       } else if (
         Number(year) === currentYear &&
         eocpQuerter === currentQuarter
       ) {
         code = 3;
-        message = "This quarter you need to update";
+        message = "This quarter you need to update.";
       } else if (Number(year) === currentYear && eocpQuerter > currentQuarter) {
         code = 4;
-        message = "You will need to update in de near future";
+        message = "You will need to update in de near future.";
       } else if (Number(year) > currentYear) {
         code = 5;
-        message = "No need to worry";
+        message = "No need to worry.";
       }
-
-      console.log({ code, message });
 
       return { code, message };
     };
@@ -101,10 +83,6 @@ const main = async () => {
         `;
     }
 
-    /**
-     * Create a comment on the PR with the information we compiled from the
-     * list of changed files.
-     */
     await octokit.rest.issues.createComment({
       owner,
       repo,
@@ -116,7 +94,7 @@ const main = async () => {
       core.setFailed(message);
     }
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error);
   }
 };
 
